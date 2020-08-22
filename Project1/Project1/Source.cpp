0971bc1d3e8e7a267a1be8b161c9328c9d290402
Ozzie07 <58475003+Ozzie07@users.noreply.github.com>
2020-08-23 02:53:45 +0900

    Update Source.cpp
    changed

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,4 +5,5 @@ int main()
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "This lecture is good" << std::endl;
 	std::cout << "I love TBCpp" << std::endl;
+	std::cout << "test" << std::endl;
 }
